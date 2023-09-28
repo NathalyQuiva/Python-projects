9044f1290a6e416fa6361349ccda7a98896291ff
NathalyQuiva <124257376+NathalyQuiva@users.noreply.github.com>
2023-09-28 10:32:17 -0500

    crazy_stories

---
+++ crazy_stories.py
@@ -0,0 +1,15 @@
+#Concatenar cadenas de caracteres.
+#Supongamos que queremos crear una cadena que diga:
+#Aprende a programar con _____________.
+
+organización = "freeCodeCamp"
+
+print("Aprende a programar con " + organización)
+
+
+
+
+
+
+
+
