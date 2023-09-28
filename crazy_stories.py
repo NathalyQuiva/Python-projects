3edc2331d12823f649d09b47571227ad2932eea7
NathalyQuiva <124257376+NathalyQuiva@users.noreply.github.com>
2023-09-28 11:08:06 -0500

    Game finished

--- crazy_stories.py
+++ crazy_stories.py
@@ -2,11 +2,26 @@
 #Supongamos que queremos crear una cadena que diga:
 #Aprende a programar con _____________.
 
-organización = "freeCodeCamp"
+# organización = "freeCodeCamp"
 
-print("Aprende a programar con " + organización)
+# print("Aprende a programar con " + organización)
+
+# print("Aprende a programas con {}".format(organización))#To replace the value of organizaions using a different syntax.
+# print (f"Aprender a programar con {organización}")#Third way to do this, using f-string
+
+#There are the Variables's definitions:
+
+adj = input("Adjetivo: ")# Yellow color indicates that input is a key word.
+verbo1 = input("Verbo: ")
+verbo2 = input("Verbo: ")
+sustantivo_plural = input("Sustantivo (plural): ")
 
 
+crazystorie = f"Programar es tan {adj}!.Siempre me emociona porque me encanta {verbo1} problemas. Aprende a {verbo2} con Nathaly Quiva y alcanza tus {sustantivo_plural}!"
+
+print(crazystorie)
+
+#Save 
 
 
 
