ebd9f21f9867d220a47ab2fb8af8d5890fc5efc4
NathalyQuiva <124257376+NathalyQuiva@users.noreply.github.com>
2023-10-02 14:49:24 -0500

    Hangman

--- hangman_game.py
+++ hangman_game.py
@@ -14,5 +14,23 @@ def ahorcado():
     print("==========================")
     print("Bienvenido(a) al juego del ahorcado")
     print("==========================")
+    
 
     palabra = obtener_palabra_válida(palabras)
+
+
+
+
+# Pregunta de la aplicacion de Python:
+# 
+# def transform(a = 2):
+#     if a == 1:
+#         return a +- 2
+#     return a
+
+# total = 1
+
+# for x in [3,5,1]:
+#     total = total + transform(x)
+
+# print(total)
