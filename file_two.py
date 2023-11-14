e8ae3c47dcec5585b2db00532f9bd4eded2295af
NathalyQuiva <124257376+NathalyQuiva@users.noreply.github.com>
2023-11-13 22:09:28 -0500

    Update practice file

--- file_two.py
+++ file_two.py
@@ -1,2 +1,6 @@
 print("File two __name__ is set up: {}".format(__name__))
 
+if __name__ == "__main__":
+    print("File two executed when ran directly")
+else:
+    print("File two executed when imported")
